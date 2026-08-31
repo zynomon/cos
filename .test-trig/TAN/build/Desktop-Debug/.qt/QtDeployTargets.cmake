1a386a9c5a610c8054159b222abf53ccd745160b
zynomon <zynomon@github.com>
2026-08-31 22:39:09 +0600

    TERM

--- .test-trig/TAN/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_terminal-angulation-navigator_FILE /home/zynomon/Documents/git/libtrigonometry/.test-trig/COT&TAN/build/Desktop-Debug/terminal-angulation-navigator)
-set(__QT_DEPLOY_TARGET_terminal-angulation-navigator_TYPE EXECUTABLE)
--- .test-trig/TAN/build/Desktop-Debug/CMakeFiles/terminal-angulation-navigator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for terminal-angulation-navigator.
-# This may be replaced when dependencies are built.
